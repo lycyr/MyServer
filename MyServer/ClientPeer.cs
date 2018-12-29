72c753251f13ebc49e4a583784bdc2ff4278aec1
ly <1449658875@qq.com>
2018-12-29 15:55:09 +0800

    初始化服务器

---
+++ MyServer/ClientPeer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photon.SocketServer;
+using PhotonHostRuntimeInterfaces;
+
+namespace MyServer
+{
+    //管理每个客户端的连接(类似客服)
+    public class ClientPeer : Photon.SocketServer.ClientPeer
+    {
+        //构造方法
+        public ClientPeer(InitRequest initRequest) : base(initRequest)
+        {
+
+        }
+        //断开连接
+        protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+        {
+
+        }
+
+        //处理客户端的请求
+        protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+        {
+
+        }
+    }
+}
---
+++ MyServer/MyServer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photon.SocketServer;
+
+
+namespace MyServer
+{
+    //所有Server端 主类
+    //函数执行顺序：Setup()->CreatePeer()->TearDown()
+    class MyServer : ApplicationBase
+    {
+        //当一个客户端请求连接时
+        //peerbase表示和一个客户端的连接 由PhotonServer完成调用
+        protected override PeerBase CreatePeer(InitRequest initRequest)
+        {
+            return new ClientPeer(initRequest);
+        }
+        //服务端应用启动的初始化操作  由PhotonServer完成调用
+        protected override void Setup()
+        {
+           
+        }
+        //server端关闭 由PhotonServer完成调用
+        protected override void TearDown()
+        {
+            
+        }
+    }
+}
